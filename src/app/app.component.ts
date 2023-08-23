5f889e7cf183b8bebf8de8b4a89e733ffe78cfac
PSKutergin <PSKutergin@gmail.com>
2023-08-23 06:43:23 +0300

    add_adaptive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AppService } from './app.service';
 
@@ -9,7 +9,11 @@ import { AppService } from './app.service';
 })
 export class AppComponent {
   currency = "$"
+  loaderShowed = true
+  loader = true
 
+  orderImageStyle: any;
+  mainImageStyle: any;
 
   form = this.fb.group({
     order: ["", Validators.required],
@@ -22,7 +26,19 @@ export class AppComponent {
   constructor(private fb: FormBuilder, private appService: AppService) {
   }
 
+  @HostListener('document:mousemove', ['$event'])
+  onMouseMove(e: MouseEvent) {
+    this.orderImageStyle = {transform: 'translate(-' + ((e.clientX * 0.3) / 8) + 'px,-' + ((e.clientY * 0.3) / 8) + 'px'}
+    this.mainImageStyle = {transform: 'translate(-' + ((e.clientX * 0.3) / 8) + 'px,-' + ((e.clientY * 0.3) / 8) + 'px'}
+  }
+
   ngOnInit() {
+    setTimeout(() => {
+      this.loaderShowed = false;
+    }, 3000);
+    setTimeout(() => {
+      this.loader = false;
+    }, 4000);
     this.appService.getData().subscribe(data => this.productsData = data)
   }
 
